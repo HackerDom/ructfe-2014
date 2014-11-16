b118785cae6980bdc782bff1b559363e33261774
Alexander Bersenev <bay@hackerdom.ru>
2014-11-16 19:22:24 +0500

    infrastructure: antiddos scripts

---
+++ infrastructure/openvpn_antidos/block_team.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+ echo "USAGE: block_team.sh team_num"
+ exit
+fi
+
+if [[ ! $1 =~ ^[0-9]+$ ]]; then
+ echo "Team num should be number"
+ exit
+fi
+
+# add the couple of rules
+iptables -t nat -A PREROUTING -m tcp -m comment --comment "antidos" -p tcp -i "tun${1}" --dport 80 -j DNAT --to-destination "10.60.${1}.1:10000"
+iptables -t nat -A PREROUTING -m tcp -m comment --comment "antidos" -p tcp -i "tun${1}" ! --dport 80 -j DNAT --to-destination "10.60.${1}.1:10001"
