5ec61d95eaa655dc6142b80de03494c3c9100d1a
Alexander Bersenev <bay@hackerdom.ru>
2014-11-25 23:56:57 +0500

    infrastructure: check if one of snat rule exists before add them all

--- infrastructure/openvpn_gamescripts/snat/add_snat_rules.sh
+++ infrastructure/openvpn_gamescripts/snat/add_snat_rules.sh
@@ -2,7 +2,18 @@
 # adds rules for teams snat. Team will see incoming connections from 10.8{0..3}.{0..255}.1
 # this script should be run once before the game starts
 
-for num in {0..1023}; do 
+# check if the rules are already exists
+for num in {0..1023}; do
+    ip="10.$((80 + num / 256)).$((num % 256)).1"
+
+    if iptables -t nat -C POSTROUTING -o team${num} -j SNAT --to-source ${ip} &>/dev/null; then
+        echo "SNAT rules already exists, delete them first"
+        exit 1
+    fi
+done
+
+
+for num in {0..1023}; do
     ip="10.$((80 + num / 256)).$((num % 256)).1"
 
     iptables -t nat -A POSTROUTING -o team${num} -j SNAT --to-source ${ip}
