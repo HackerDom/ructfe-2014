5197a8833fbf182978ca8d44fc1ee44c73fdac5b
Alexander Bersenev <bay@hackerdom.ru>
2014-11-25 23:48:42 +0500

    infrastructure: typo in last commit

--- infrastructure/openvpn_gamescripts/snat/del_snat_rules.sh
+++ infrastructure/openvpn_gamescripts/snat/del_snat_rules.sh
@@ -5,5 +5,5 @@
 for num in {0..1023}; do 
     ip="10.$((80 + num / 256)).$((num % 256)).1"
 
-    iptables -t nat -D POSTROUTING -o team${num} -j SNAT --to-source ${num}
+    iptables -t nat -D POSTROUTING -o team${num} -j SNAT --to-source ${ip}
 done
