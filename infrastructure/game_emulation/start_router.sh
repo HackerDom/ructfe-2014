a2517367ff7e3d6470b98d48ff5b6539069b60af
Alexander Bersenev <bay@hackerdom.ru>
2014-11-28 15:44:04 +0500

    infrastructure: add one more network bridge to decrease network load

--- infrastructure/game_emulation/start_router.sh
+++ infrastructure/game_emulation/start_router.sh
@@ -13,9 +13,23 @@ if docker stop ${full_name} &>/dev/null; then
     docker rm ${full_name}
 fi
 
-cid=$(docker run --privileged --name ${full_name} --memory=2048m --cpu-shares=10 -d ructfe2014:${name} /usr/sbin/openvpn /etc/openvpn/${team_num}.conf)
+cid=$(docker run --net=none --privileged --name ${full_name} --memory=2048m --cpu-shares=10 -d ructfe2014:${name} /usr/sbin/openvpn /etc/openvpn/${team_num}.conf)
 
 ip="10.$((60 + team_num / 256)).$((team_num % 256)).1/24"
+bridged_ip="10.$((100 + team_num / 256)).$((team_num % 256)).2/24"
+bridged_router="10.$((100 + team_num / 256)).$((team_num % 256)).1"
 
 # add eth1 interface
 pipework/pipework br$((team_num + 10000)) ${cid} ${ip}
+
+# set eth0 interface
+pipework/pipework br$((team_num + 20000)) -i eth0 ${cid} ${bridged_ip}@${bridged_router}
+ip addr add ${bridged_router}/24 dev br$((team_num + 20000))
+
+if ! iptables -t nat -C POSTROUTING -o eth0 -j MASQUERADE &>/dev/null; then
+ echo "Autoexecuting: iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE"
+ echo "If you don't need it, comment it"
+ iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+fi
+
+# do not forget do iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE on the host computer
