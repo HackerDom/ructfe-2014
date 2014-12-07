2a75a86e6bed145c90209b5e082e784f6c5ae858
Konstantin Plotnikov <kost@hackerdom.ru>
2014-12-08 00:58:02 +0500

    Small fix

--- checksystem/db/ructfe2014-services.sql
+++ checksystem/db/ructfe2014-services.sql
@@ -1,3 +1,3 @@
--- RuCTF 2014
+-- RuCTFE 2014
 
-INSERT INTO services (id,name,checker,is_not_task,vulnbox_suffix) VALUES ( 1, 'Test'      , '' , true, 2);
+INSERT INTO services (id,name,checker,is_not_task,vulnbox_suffix) VALUES ( 1, 'VWS'      , '' , true, 2);
