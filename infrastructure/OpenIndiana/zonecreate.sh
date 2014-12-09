fd829d08bbdf04f044eeddd715575d6d5a2d0032
m_messiah <m.muzafarov@gmail.com>
2014-12-09 21:38:37 +0500

    openindiana: fix zonecreate

--- infrastructure/OpenIndiana/zonecreate.sh
+++ infrastructure/OpenIndiana/zonecreate.sh
@@ -4,9 +4,13 @@
 zonename="khozov"
 #ipaddr=$2
 ipaddr=8
+mkdir /export/$zonepath
+chown root:root /export/$zonepath
+chmod 700 /export/$zonepath
+
 zonecfg -z $zonename << EOF
     create -b
-    set dir=/export/$zonename
+    set zonepath=/export/$zonename
     set autoboot=true
     add net
       set physical=xnf0
@@ -15,7 +19,8 @@ zonecfg -z $zonename << EOF
     verify
     commit
     exit
-  EOF
-  zoneadm -z $zonename install
-  zoneadm -z $zonename boot
-  zlogin -C $zonename
+EOF
+
+zoneadm -z $zonename install
+zoneadm -z $zonename boot
+zlogin -C $zonename
