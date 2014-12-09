cd70f60c5bde7bc648743c01a71e6b121154e118
Alexander Bersenev <bay@hackerdom.ru>
2014-12-09 23:44:03 +0500

    infrastructure: use real teams in monitor

--- infrastructure/vpn/status/teams.py
+++ infrastructure/vpn/status/teams.py
@@ -1,7 +1,19 @@
-teams = {
-	0: "test_team1",
-	1: "dream_team"
-}
+import requests
+import json
 
-for i in range(768):
-	teams[i] = "test_team%d" % i
+USE_REAL_TEAMS = True
+
+URL = "http://ructf.org/e/2014/teams/info"
+
+def get_teams():
+	teams = {}
+	if not USE_REAL_TEAMS:
+		for i in range(768):
+			teams[i] = "test_team%d" % i
+	else:
+		content = requests.get(URL).text
+		teams_list = json.loads(content)
+		for team_id, team_name in teams_list:
+			if type(team_id) is int:
+				teams[team_id] = team_name
+	return teams
