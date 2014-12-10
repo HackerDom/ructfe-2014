126efe60d9db3360562578fa569ba8b16049c48b
Alexander Bersenev <bay@hackerdom.ru>
2014-12-10 22:25:33 +0500

    infrastructure: various typos in game emulation

--- infrastructure/game_emulation/start_s5.sh
+++ infrastructure/game_emulation/start_s5.sh
@@ -13,7 +13,7 @@ if docker stop ${full_name} &>/dev/null; then
     docker rm ${full_name}
 fi
 
-cid=$(docker run --net=none --name ${full_name} --memory=2048m --cpu-shares=10 -d ructfe2014:${name} /usr/bin/python3.4 /root/service.py 2000 1000000 0)
+cid=$(docker run --net=none --name ${full_name} --memory=2048m --cpu-shares=10 -d ructfe2014:${name} /usr/bin/python3.4 /root/service.py 5555 1000000 0)
 
 ip="10.$((60 + team_num / 256)).$((team_num % 256)).105/24"
 router_ip="10.$((60 + team_num / 256)).$((team_num % 256)).1"
