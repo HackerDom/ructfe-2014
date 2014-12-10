baa6e7af8479d3774e15747c800153f66b92e2ee
Alexander Bersenev <bay@hackerdom.ru>
2014-12-10 22:24:42 +0500

    infrastructure: add my sshd public key to new emulation instances

--- infrastructure/game_emulation/gcloud_create_instance.sh
+++ infrastructure/game_emulation/gcloud_create_instance.sh
@@ -33,7 +33,7 @@ done
 EOF
 
 echo Making an instance
-gcloud --project="$project" compute instances create "$v_name" --zone "$zone" --machine-type "n1-standard-1" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1404-trusty-v20141031a" --boot-disk-type "pd-standard" --metadata-from-file startup-script="$startup_file"
+gcloud --project="$project" compute instances create "$v_name" --zone "$zone" --machine-type "n1-standard-1" --metadata "sshKeys=root:ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAv8TShkTzuSLDTxHnzvs8/8jg6CrN7DYM9rpS6GswaqPeW6jkSap931S5WoR93tFX025dbU5/wExZ9bFc2GaaMVhxFfEE9eP2Y6rUmqcCdlT4ywXgPn2d4WGVCCCk9/VPZRqnWkBoh4aQnkS9muqwnkcJefQavTjoQur1z6T0cL1x8tfORRCyi7to9IiA4c49JuLN2Py+UJKks7AdjEmkBjjpTbOZNudyNzFH9ntPS9t8ELcF9ioy5jv6f/sGrEsGTOskS7czOI543rwZwC4XFCcH7B5ib8jb0DofN7gRHsmvw+SntJr7KGAUdgbFbbzeueEMfZ000Umr52jxyqMUeQ== root@BAYsGentooNotebook" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1404-trusty-v20141031a" --boot-disk-type "pd-standard" --metadata-from-file startup-script="$startup_file"
 
 echo "Copying files"
 until gcloud --project="$project" compute copy-files ../game_emulation "ubuntu@$v_name:/home/ubuntu/" --zone "$zone"; do
