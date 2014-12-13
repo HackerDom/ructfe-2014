7162faa165e3b2c1ac4433194da0af01cd6008d7
Andrey Khozov <avkhozov@gmail.com>
2014-12-13 22:24:37 +0500

    [monitor] Use Mojo::Pg instead of M::P::PgAsync

--- checksystem/monitor/lib/Monitor.pm
+++ checksystem/monitor/lib/Monitor.pm
@@ -2,6 +2,7 @@ package Monitor;
 use Mojo::Base 'Mojolicious';
 
 use Mojo::Util 'slurp';
+use Mojo::Pg;
 use Time::Piece;
 
 has services   => sub { {} };
@@ -12,105 +13,107 @@ has status     => sub { {} };
 has flags      => sub { {} };
 has history    => sub { [] };
 
-has ip2team    => sub { {} };
+has ip2team => sub { {} };
 
 sub startup {
-  my $self = shift;
+  my $app = shift;
 
-  my $mode = $self->mode;
-  $self->plugin('Config', file => "monitor.$mode.conf");
+  my $mode = $app->mode;
+  $app->plugin('Config', file => "monitor.$mode.conf");
 
-  $self->plugin(
-    'PgAsync',
-    dbi => [
-      $self->config->{db}{source}, $self->config->{db}{user},
-      $self->config->{db}{pass}, {AutoCommit => 1, RaiseError => 1, pg_enable_utf8 => 1}]);
+  $app->helper(
+    pg => sub {
+      state $pg = Mojo::Pg->new($app->config->{db});
+    });
 
-  my $r = $self->routes;
+  my $r = $app->routes;
   $r->get('/')->to('main#index')->name('index');
   $r->get('/flags')->to('main#flags')->name('flags');
   $r->get('/history')->to('main#history')->name('history');
 
-  $self->pg(
-    'SELECT id, name FROM services;',
+  $app->log->info('Fetch services at startup');
+  $app->pg->db->query(
+    'SELECT id, name FROM services',
     sub {
-      my $db = shift;
+      my ($db, $err, $res) = @_;
+      return $app->log->error("Error while select services: $err") if $err;
 
-      $self->log->info('Fetch services at startup');
-      while (my $row = $db->sth->fetchrow_hashref()) {
-        $self->services->{$row->{id}} = $row->{name};
-      }
+      $res->arrays->map(sub { $app->services->{$_->[0]} = $_->[1] });
     });
 
-  $self->pg(
-    'SELECT id, name, vuln_box FROM teams;',
+  $app->log->info('Fetch teams at startup');
+  $app->pg->db->query(
+    'SELECT id, name, vuln_box FROM teams',
     sub {
-      my $db = shift;
+      my ($db, $err, $res) = @_;
+      return $app->log->error("Error while select services: $err") if $err;
 
-      $self->log->info('Fetch teams at startup');
-      while (my $row = $db->sth->fetchrow_hashref()) {
-        $self->teams->{$row->{id}} = $row;
-        $self->ip2team->{$row->{vuln_box}} = $row->{name};
-      }
+      $res->hashes->map(
+        sub {
+          $app->teams->{$_->{id}}         = $_;
+          $app->ip2team->{$_->{vuln_box}} = $_->{name};
+        });
     });
 
   Mojo::IOLoop->recurring(
     30 => sub {
-      $self->log->info('Update scoreboard');
+      $app->log->info('Update scoreboard');
       Mojo::IOLoop->delay(
         sub {
           my $delay = shift;
 
-          $self->log->info('Fetch SLA and FP for all team');
-          $self->pg(
+          $app->pg->db->query(
             'SELECT DISTINCT ON (team_id, service_id) team_id, service_id, score
-            FROM score ORDER BY team_id, service_id, time DESC;'
+            FROM score ORDER BY team_id, service_id, time DESC'
               => $delay->begin
           );
-          $self->pg(
+          $app->pg->db->query(
             'SELECT DISTINCT ON (team_id, service_id) team_id, service_id, successed, failed
-            FROM sla ORDER BY team_id, service_id, time DESC;'
+            FROM sla ORDER BY team_id, service_id, time DESC'
               => $delay->begin
           );
-          $self->pg(
+          $app->pg->db->query(
             'SELECT n, EXTRACT(EPOCH FROM time) AS time
-            FROM rounds ORDER BY n DESC LIMIT 1;'
+            FROM rounds ORDER BY n DESC LIMIT 1'
               => $delay->begin
           );
-          $self->pg(
-            'SELECT team_id, service_id, status, fail_comment FROM service_status;'
-              => $delay->begin
-          );
-          $self->pg(
-            'SELECT * FROM services_flags_stolen;'
-              => $delay->begin
-          );
+          $app->pg->db->query(
+            'SELECT team_id, service_id, status, fail_comment FROM service_status' =>
+              $delay->begin);
+          $app->pg->db->query('SELECT * FROM services_flags_stolen' => $delay->begin);
         },
         sub {
-          my ($delay, $fh, $sh, $rh, $ssh, $flh) = @_;
+          my ($delay, $e1, $scores, $e2, $services, $e3, $round, $e4, $status, $e5, $flags) = @_;
+          if (my $e = $e1 || $e2 || $e3 || $e4 || $e5) {
+            $app->log->error("Error while update scoreboard: $e");
+            return;
+          }
+
           my ($flag_points, $sla_points);
 
-          while (my $row = $sh->sth->fetchrow_hashref()) {
-            my ($sla, $sum) = (1, $row->{successed} + $row->{failed});
-            $sla = $row->{successed} / $sum if $sum > 0;
-            $sla_points->{$row->{team_id}}{$row->{service_id}} = $sla;
-          }
-          while (my $row = $fh->sth->fetchrow_hashref) {
-            $flag_points->{$row->{team_id}}{$row->{service_id}} = $row->{score};
-          }
+          $services->hashes->map(
+            sub {
+              my ($sla, $sum) = (1, $_->{successed} + $_->{failed});
+              $sla = $_->{successed} / $sum if $sum > 0;
+              $sla_points->{$_->{team_id}}{$_->{service_id}} = $sla;
+            });
+
+          $scores->hashes->map(
+            sub {
+              $flag_points->{$_->{team_id}}{$_->{service_id}} = $_->{score};
+            });
 
           my @data;
-          for my $tid (keys %{$self->teams}) {
+          for my $tid (keys %{$app->teams}) {
 
             my $score = 0;
-            $score += $sla_points->{$tid}{$_} * $flag_points->{$tid}{$_}
-              for keys %{$self->services};
+            $score += $sla_points->{$tid}{$_} * $flag_points->{$tid}{$_} for keys %{$app->services};
 
             push @data, {
               team => {
                 id       => $tid,
-                name     => $self->teams->{$tid}{name},
-                vuln_box => $self->teams->{$tid}{vuln_box}
+                name     => $app->teams->{$tid}{name},
+                vuln_box => $app->teams->{$tid}{vuln_box}
               },
               sla   => $sla_points->{$tid},
               fp    => $flag_points->{$tid},
@@ -119,40 +122,46 @@ sub startup {
           }
 
           @data = sort { $b->{score} <=> $a->{score} } @data;
-          $self->scoreboard(\@data);
+          $app->scoreboard(\@data);
 
-          my $row = $rh->sth->fetchrow_hashref;
-          $self->round({n => $row->{n}, time => scalar localtime int $row->{time}});
+          my $r = $round->hash;
+          $app->round({n => $r->{n}, time => scalar localtime int $r->{time}});
 
-          while (my $row = $ssh->sth->fetchrow_hashref()) {
-            $self->status->{$row->{service_id}}{$row->{team_id}} = $row;
-          }
+          $status->hashes->map(
+            sub {
+              $app->status->{$_->{service_id}}{$_->{team_id}} = $_;
+            });
 
-          while (my $row = $flh->sth->fetchrow_hashref()) {
-            $self->flags->{$row->{team_id}}{$row->{service_id}} =
-              {count => $row->{flags}, name => $row->{service}};
-          }
+          $flags->hashes->map(
+            sub {
+              $app->flags->{$_->{team_id}}{$_->{service_id}} =
+                {count => $_->{flags}, name => $_->{service}};
+            });
         });
     });
-    Mojo::IOLoop->recurring(
-      120 => sub {
-        $self->log->info('Update history');
-        $self->pg(
-          'SELECT * FROM points_history
-          ORDER BY team_id, round' => sub {
-            my $ph = shift;
-            my ($h, $nh);
-            while (my $row = $ph->sth->fetchrow_hashref()) {
-              if (($row->{round} > $self->round->{n} - 40) or ($row->{round} % 15 == 0)) {
-                push @{$h->{$row->{name}}}, {x => $row->{round}, y => 0 + $row->{points}};
+
+  Mojo::IOLoop->recurring(
+    120 => sub {
+      $app->log->info('Update history');
+      $app->pg->db->query(
+        'SELECT * FROM points_history
+        ORDER BY team_id, round' => sub {
+          my ($db, $err, $res) = @_;
+          return $app->log->error("Error while select services: $err") if $err;
+
+          my ($h, $nh);
+          $res->hashes->map(
+            sub {
+              if (($_->{round} > $app->round->{n} - 40) or ($_->{round} % 15 == 0)) {
+                push @{$h->{$_->{name}}}, {x => $_->{round}, y => 0 + $_->{points}};
               }
-            }
-            for (keys %$h) {
-              push @$nh, {name => $_, data => $h->{$_}};
-            }
-            $self->history($nh);
+            });
+
+          for (keys %$h) {
+            push @$nh, {name => $_, data => $h->{$_}};
           }
-        );
+          $app->history($nh);
+        });
     });
 }
 
