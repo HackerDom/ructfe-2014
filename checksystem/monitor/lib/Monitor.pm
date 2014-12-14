ef4473ee458959ddfb6cb1fe52eff976dfb9b34b
Andrey Khozov <avkhozov@gmail.com>
2014-12-14 17:09:39 +0500

    [monitor] Update scoreboard data at startup

--- checksystem/monitor/lib/Monitor.pm
+++ checksystem/monitor/lib/Monitor.pm
@@ -13,6 +13,8 @@ has status     => sub { {} };
 has flags      => sub { {} };
 has ip2team    => sub { {} };
 
+has lock => 0;
+
 sub startup {
   my $app = shift;
 
@@ -53,90 +55,94 @@ sub startup {
         });
     });
 
-  Mojo::IOLoop->recurring(
-    30 => sub {
-      $app->log->info('Update scoreboard');
-      Mojo::IOLoop->delay(
-        sub {
-          my $delay = shift;
+  my $update = sub {
+    return if $app->lock;
+    $app->lock(1);
+    $app->log->info('Update scoreboard');
+    Mojo::IOLoop->delay(
+      sub {
+        my $delay = shift;
 
-          $app->pg->db->query(
-            'SELECT DISTINCT ON (team_id, service_id) team_id, service_id, score
+        $app->pg->db->query(
+          'SELECT DISTINCT ON (team_id, service_id) team_id, service_id, score
             FROM score ORDER BY team_id, service_id, time DESC'
-              => $delay->begin
-          );
-          $app->pg->db->query(
-            'SELECT DISTINCT ON (team_id, service_id) team_id, service_id, successed, failed
+            => $delay->begin
+        );
+        $app->pg->db->query(
+          'SELECT DISTINCT ON (team_id, service_id) team_id, service_id, successed, failed
             FROM sla ORDER BY team_id, service_id, time DESC'
-              => $delay->begin
-          );
-          $app->pg->db->query(
-            'SELECT n, EXTRACT(EPOCH FROM time) AS time
+            => $delay->begin
+        );
+        $app->pg->db->query(
+          'SELECT n, EXTRACT(EPOCH FROM time) AS time
             FROM rounds ORDER BY n DESC LIMIT 1'
-              => $delay->begin
-          );
-          $app->pg->db->query(
-            'SELECT team_id, service_id, status, fail_comment FROM service_status' =>
-              $delay->begin);
-          $app->pg->db->query('SELECT * FROM services_flags_stolen' => $delay->begin);
-        },
-        sub {
-          my ($delay, $e1, $scores, $e2, $services, $e3, $round, $e4, $status, $e5, $flags) = @_;
-          if (my $e = $e1 || $e2 || $e3 || $e4 || $e5) {
-            $app->log->error("Error while update scoreboard: $e");
-            return;
-          }
+            => $delay->begin
+        );
+        $app->pg->db->query(
+          'SELECT team_id, service_id, status, fail_comment FROM service_status' => $delay->begin);
+        $app->pg->db->query('SELECT * FROM services_flags_stolen' => $delay->begin);
+      },
+      sub {
+        my ($delay, $e1, $scores, $e2, $services, $e3, $round, $e4, $status, $e5, $flags) = @_;
+        $app->lock(0);
+        if (my $e = $e1 || $e2 || $e3 || $e4 || $e5) {
+          $app->log->error("Error while update scoreboard: $e");
+          return;
+        }
 
-          my ($flag_points, $sla_points);
+        my ($flag_points, $sla_points);
 
-          $services->hashes->map(
-            sub {
-              my ($sla, $sum) = (1, $_->{successed} + $_->{failed});
-              $sla = $_->{successed} / $sum if $sum > 0;
-              $sla_points->{$_->{team_id}}{$_->{service_id}} = $sla;
-            });
+        $services->hashes->map(
+          sub {
+            my ($sla, $sum) = (1, $_->{successed} + $_->{failed});
+            $sla = $_->{successed} / $sum if $sum > 0;
+            $sla_points->{$_->{team_id}}{$_->{service_id}} = $sla;
+          });
 
-          $scores->hashes->map(
-            sub {
-              $flag_points->{$_->{team_id}}{$_->{service_id}} = $_->{score};
-            });
+        $scores->hashes->map(
+          sub {
+            $flag_points->{$_->{team_id}}{$_->{service_id}} = $_->{score};
+          });
 
-          my @data;
-          for my $tid (keys %{$app->teams}) {
+        my @data;
+        for my $tid (keys %{$app->teams}) {
 
-            my $score = 0;
-            $score += $sla_points->{$tid}{$_} * $flag_points->{$tid}{$_} for keys %{$app->services};
+          my $score = 0;
+          $score += $sla_points->{$tid}{$_} * $flag_points->{$tid}{$_} for keys %{$app->services};
 
-            push @data, {
-              team => {
-                id       => $tid,
-                name     => $app->teams->{$tid}{name},
-                vuln_box => $app->teams->{$tid}{vuln_box}
-              },
-              sla   => $sla_points->{$tid},
-              fp    => $flag_points->{$tid},
-              score => $score
-              };
-          }
+          push @data, {
+            team => {
+              id       => $tid,
+              name     => $app->teams->{$tid}{name},
+              vuln_box => $app->teams->{$tid}{vuln_box}
+            },
+            sla   => $sla_points->{$tid},
+            fp    => $flag_points->{$tid},
+            score => $score
+            };
+        }
 
-          @data = sort { $b->{score} <=> $a->{score} } @data;
-          $app->scoreboard(\@data);
+        @data = sort { $b->{score} <=> $a->{score} } @data;
+        $app->scoreboard(\@data);
 
-          my $r = $round->hash;
-          $app->round({n => $r->{n}, time => scalar localtime int $r->{time}});
+        my $r = $round->hash;
+        $app->round({n => $r->{n}, time => scalar localtime int $r->{time}});
 
-          $status->hashes->map(
-            sub {
-              $app->status->{$_->{service_id}}{$_->{team_id}} = $_;
-            });
+        $status->hashes->map(
+          sub {
+            $app->status->{$_->{service_id}}{$_->{team_id}} = $_;
+          });
 
-          $flags->hashes->map(
-            sub {
-              $app->flags->{$_->{team_id}}{$_->{service_id}} =
-                {count => $_->{flags}, name => $_->{service}};
-            });
-        });
-    });
+        $flags->hashes->map(
+          sub {
+            $app->flags->{$_->{team_id}}{$_->{service_id}} =
+              {count => $_->{flags}, name => $_->{service}};
+          });
+      });
+  };
+  $update->();
+
+  Mojo::IOLoop->recurring(30 => $update);
 }
 
 1;
