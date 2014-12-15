8cebd3a7de41ec682f50eede428c5ea0daee56fc
Alexander Bersenev <bay@hackerdom.ru>
2014-12-15 15:13:50 +0500

    infrastructure: add flag posting in the game emulation

--- infrastructure/game_emulation/gcloud_create_teams.sh
+++ infrastructure/game_emulation/gcloud_create_teams.sh
@@ -25,32 +25,32 @@ project=${1?usage: ./gcloud_create_teams <project>}
 #./gcloud_create_instance.sh $project europe-west1-c 192 224 &
 #./gcloud_create_instance.sh $project europe-west1-c 224 256 &
 
-#./gcloud_create_instance.sh $project europe-west1-c 128 144 &
-#./gcloud_create_instance.sh $project europe-west1-c 144 160 &
-#./gcloud_create_instance.sh $project europe-west1-c 160 176 &
-#./gcloud_create_instance.sh $project europe-west1-c 176 192 &
-#./gcloud_create_instance.sh $project europe-west1-c 192 208 &
-#./gcloud_create_instance.sh $project europe-west1-c 208 224 &
-#./gcloud_create_instance.sh $project europe-west1-c 224 240 &
-#./gcloud_create_instance.sh $project europe-west1-c 240 256 &
+./gcloud_create_instance.sh $project europe-west1-c 128 144 &
+./gcloud_create_instance.sh $project europe-west1-c 144 160 &
+./gcloud_create_instance.sh $project europe-west1-c 160 176 &
+./gcloud_create_instance.sh $project europe-west1-c 176 192 &
+./gcloud_create_instance.sh $project europe-west1-c 192 208 &
+./gcloud_create_instance.sh $project europe-west1-c 208 224 &
+./gcloud_create_instance.sh $project europe-west1-c 224 240 &
+./gcloud_create_instance.sh $project europe-west1-c 240 256 &
  
-#./gcloud_create_instance.sh $project asia-east1-c 256 272 &
-#./gcloud_create_instance.sh $project asia-east1-c 272 288 &
-#./gcloud_create_instance.sh $project asia-east1-c 288 304 &
-#./gcloud_create_instance.sh $project asia-east1-c 304 320 &
-#./gcloud_create_instance.sh $project asia-east1-c 320 336 &
-#./gcloud_create_instance.sh $project asia-east1-c 336 352 &
-#./gcloud_create_instance.sh $project asia-east1-c 352 368 &
-#./gcloud_create_instance.sh $project asia-east1-c 368 384 &
+./gcloud_create_instance.sh $project asia-east1-c 256 272 &
+./gcloud_create_instance.sh $project asia-east1-c 272 288 &
+./gcloud_create_instance.sh $project asia-east1-c 288 304 &
+./gcloud_create_instance.sh $project asia-east1-c 304 320 &
+./gcloud_create_instance.sh $project asia-east1-c 320 336 &
+./gcloud_create_instance.sh $project asia-east1-c 336 352 &
+./gcloud_create_instance.sh $project asia-east1-c 352 368 &
+./gcloud_create_instance.sh $project asia-east1-c 368 384 &
  
-#./gcloud_create_instance.sh $project us-central1-f 384 400 &
-#./gcloud_create_instance.sh $project us-central1-f 400 416 &
-#./gcloud_create_instance.sh $project us-central1-f 416 432 &
-#./gcloud_create_instance.sh $project us-central1-f 432 448 &
-#./gcloud_create_instance.sh $project us-central1-f 448 464 &
-#./gcloud_create_instance.sh $project us-central1-f 464 480 &
-#./gcloud_create_instance.sh $project us-central1-f 480 496 &
-#./gcloud_create_instance.sh $project us-central1-f 496 512 &
+./gcloud_create_instance.sh $project us-central1-f 384 400 &
+./gcloud_create_instance.sh $project us-central1-f 400 416 &
+./gcloud_create_instance.sh $project us-central1-f 416 432 &
+./gcloud_create_instance.sh $project us-central1-f 432 448 &
+./gcloud_create_instance.sh $project us-central1-f 448 464 &
+./gcloud_create_instance.sh $project us-central1-f 464 480 &
+./gcloud_create_instance.sh $project us-central1-f 480 496 &
+./gcloud_create_instance.sh $project us-central1-f 496 512 &
 
 ./gcloud_create_instance.sh $project europe-west1-c 512 528 &
 ./gcloud_create_instance.sh $project europe-west1-c 528 544 &
