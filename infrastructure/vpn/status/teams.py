ba8f395b74da69ddfe159b5121bc795fe20106df
Alexander Bersenev <bay@hackerdom.ru>
2014-12-15 22:34:05 +0500

    infrastructure: more reliable teams list in teamcheck

--- infrastructure/vpn/status/teams.py
+++ infrastructure/vpn/status/teams.py
@@ -5,15 +5,23 @@ USE_REAL_TEAMS = True
 
 URL = "http://ructf.org/e/2014/teams/info"
 
+teams_cache = {}
+
 def get_teams():
-	teams = {}
-	if not USE_REAL_TEAMS:
-		for i in range(768):
-			teams[i] = "test_team%d" % i
-	else:
-		content = requests.get(URL).text
-		teams_list = json.loads(content)
-		for team_id, team_name in teams_list:
-			if type(team_id) is int:
-				teams[team_id] = team_name
+	global teams_cache
+	try:
+		teams = {}
+		if not USE_REAL_TEAMS:
+			for i in range(768):
+				teams[i] = "test_team%d" % i
+		else:
+				content = requests.get(URL).text
+				teams_list = json.loads(content)
+				for team_id, team_name in teams_list:
+					if type(team_id) is int:
+						teams[team_id] = team_name
+	except Exception:
+		return teams_cache
+
+	teams_cache = teams
 	return teams
