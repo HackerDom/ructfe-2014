1b4ef525643f041c81bb2acf4cf41d94fc5bd454
Konstantin Plotnikov <kost@hackerdom.ru>
2014-12-17 20:32:26 +0500

    Checksystem config

--- checksystem/db/ructfe2014-services.sql
+++ checksystem/db/ructfe2014-services.sql
@@ -1,6 +1,6 @@
 -- RuCTFE 2014
 INSERT INTO services (id,name,checker,is_not_task,vulnbox_suffix) VALUES ( 1, 'VWS'      , '' , true, 5);
---INSERT INTO services (id,name,checker,is_not_task,vulnbox_suffix) VALUES ( 2, 'Heart'      , '' , true, 6);
+INSERT INTO services (id,name,checker,is_not_task,vulnbox_suffix) VALUES ( 2, 'Heart'      , '' , true, 6);
 INSERT INTO services (id,name,checker,is_not_task,vulnbox_suffix) VALUES ( 3, 'Pidometer'      , '' , true, 7);
 INSERT INTO services (id,name,checker,is_not_task,vulnbox_suffix) VALUES ( 4, 'Glass'      , '' , true, 8);
 INSERT INTO services (id,name,checker,is_not_task,vulnbox_suffix) VALUES ( 5, 'Voicebox'      , '' , true, 214);--WARN change suffix
