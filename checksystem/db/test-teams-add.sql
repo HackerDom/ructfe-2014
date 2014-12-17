c71eee1a631b8313dc8332ffffadcd1d9f98a267
Konstantin Plotnikov <kost@hackerdom.ru>
2014-12-17 17:04:15 +0500

    Fix

--- checksystem/db/test-teams-add.sql
+++ checksystem/db/test-teams-add.sql
@@ -1,3 +1,3 @@
 INSERT INTO teams (id,name,network,vuln_box,enabled) VALUES (1, 'Team7', '10.60.7.0/24', '10.60.7.0', true);
-INSERT INTO teams (id,name,network,vuln_box,enabled) VALUES (1, 'TeamUrfu', '172.16.19.0/24', '172.16.19.0', true);
-INSERT INTO teams (id,name,network,vuln_box,enabled) VALUES (1, 'Team42', '10.60.42.0/24', '10.60.42.0', true);
+INSERT INTO teams (id,name,network,vuln_box,enabled) VALUES (2, 'TeamUrfu', '172.16.19.0/24', '172.16.19.0', true);
+INSERT INTO teams (id,name,network,vuln_box,enabled) VALUES (3, 'Team42', '10.60.42.0/24', '10.60.42.0', true);
