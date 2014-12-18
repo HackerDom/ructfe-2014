ea41347a9def804c1f98b3af04f7ba5a40666e06
Konstantin Plotnikov <kost@hackerdom.ru>
2014-12-19 01:16:43 +0500

    New test team

--- checksystem/db/test-teams-add.sql
+++ checksystem/db/test-teams-add.sql
@@ -3,3 +3,4 @@ INSERT INTO teams (id,name,network,vuln_box,enabled) VALUES (2, 'Team13', '10.60
 INSERT INTO teams (id,name,network,vuln_box,enabled) VALUES (3, 'Team42', '10.60.42.0/24', '10.60.42.0', true);
 INSERT INTO teams (id,name,network,vuln_box,enabled) VALUES (4, 'Team43', '10.60.43.0/24', '10.60.43.0', true);
 INSERT INTO teams (id,name,network,vuln_box,enabled) VALUES (5, 'Team44', '10.60.44.0/24', '10.60.44.0', true);
+INSERT INTO teams (id,name,network,vuln_box,enabled) VALUES (6, 'Team157', '10.60.157.0/24', '10.60.157.0', true);
