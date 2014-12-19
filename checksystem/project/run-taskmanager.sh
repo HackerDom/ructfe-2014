df83260c2e705d7285c317829d842c3dd2560615
Konstantin Plotnikov <kost@hackerdom.ru>
2014-12-20 00:40:51 +0500

    Update checksystem libs

--- checksystem/project/run-taskmanager.sh
+++ checksystem/project/run-taskmanager.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -classpath .:external/postgresql-8.4-701.jdbc4.jar:external/junit-4.6.jar:external/log4j-1.2.15.jar ructf.taskManager.Main checksystem.cfg
+java -classpath .:external/postgresql-9.3-1102.jdbc41.jar:external/junit-4.6.jar:external/log4j-1.2.17.jar ructf.taskManager.Main checksystem.cfg
