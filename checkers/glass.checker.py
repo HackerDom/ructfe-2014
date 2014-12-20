339411b2cd5438ec3776b060d6f8c4984eff3f71
pahaz <pahaz.blinov@gmail.com>
2014-12-20 17:02:00 +0500

    glass: checker change statuses

--- checkers/glass.checker.py
+++ checkers/glass.checker.py
@@ -168,11 +168,11 @@ def main():
     try:
         {'check': chk.check, 'put': chk.put, 'get': chk.get}[mode](id_, flag)
     except urllib.error.HTTPError as e:
-        ServiceChecker._done(ExitCode.DOWN, "exitcode={}".format(e.getcode()))
+        ServiceChecker._done(ExitCode.MUMBLE, "exitcode={}".format(e.getcode()))
     except urllib.error.URLError as e:
         ServiceChecker._done(ExitCode.DOWN, ServiceChecker.NO_CONNECT)
     except Exception as e:
-        ServiceChecker._done(ExitCode.DOWN, "DOWN", str(e))
+        ServiceChecker._done(ExitCode.MUMBLE, "WTF", str(e))
 
 
 if __name__ == '__main__':
